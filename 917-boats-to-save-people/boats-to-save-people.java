39072769ddaa034ba0629d21adbe6f44858e1046
Rahul Asapu <asapurahul393@gmail.com>
2026-08-19 17:30:00 +0530

    Time: 20 ms (83.26%) | Memory: 56.3 MB (77.24%) - LeetSync

---
+++ 917-boats-to-save-people/boats-to-save-people.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        int c=0;
+        Arrays.sort(people);
+        int i=0;
+        int j=people.length-1;
+        while(i<=j)
+        {
+            if(people[i]+people[j]<=limit)
+                i++;
+            j--;
+            c++;
+        }
+        return c;
+    }
+}
