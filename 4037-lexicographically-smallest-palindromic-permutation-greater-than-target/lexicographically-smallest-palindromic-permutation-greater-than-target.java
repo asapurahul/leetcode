a9d2e334d97614d7c3cee6bd6256a5883321cda5
Rahul Asapu <asapurahul393@gmail.com>
2026-08-28 17:50:56 +0530

    Time: 4 ms (100.00%) | Memory: 46.7 MB (66.67%) - LeetSync

--- 4037-lexicographically-smallest-palindromic-permutation-greater-than-target/lexicographically-smallest-palindromic-permutation-greater-than-target.java
+++ 4037-lexicographically-smallest-palindromic-permutation-greater-than-target/lexicographically-smallest-palindromic-permutation-greater-than-target.java
@@ -1,69 +1,123 @@
 class Solution {
     public String lexPalindromicPermutation(String s, String target) {
-        int[] freq = new int[26];
-        for (int i = 0; i < s.length(); i++)
-            freq[s.charAt(i) - 'a']++;
+        int[] a = new int[26];
+        for (char ch : s.toCharArray()){
+            a[ch - 'a']++;
+        }
+        char mid = '.';
+        for (int i = 0; i < 26; i++){
+            if ((a[i]&1) == 1){
+                if (mid != '.')return "";
+                mid = (char) (i + (int)'a');
+            }
+            a[i] /= 2;
+        }
+        StringBuilder str = new StringBuilder();
+        int flag = 0;
+        for (int i = 0; i < s.length() / 2; i++){
+            char ch = target.charAt(i);
+            if (flag == 0){
+                boolean ok  = false;
+                if (a[ch - 'a'] > 0){
+                    a[ch - 'a']--;
+                    str.append(ch);
+                    continue;
+                }
+                for (int j = ch - 'a'; j < 26; j++){
+                    if (a[j] > 0){
+                        a[j]--;
+                        ok =true;
+                        str.append((char) (j + 'a'));
+                        break;
+                    }
+                }
+                if (ok){
+                    flag = 1;
+                    continue;
+                }
+                for (int j = i - 1; j >= 0 && !ok; j--){
+                    for (int k = 0; k < 26; k++){
+    
+                        if (k > str.charAt(j) - 'a' && a[k] > 0){
+                            a[str.charAt(j) - 'a']++;
+                            str.setCharAt(j, (char) (k + 'a'));
+                            a[k]--;
+                            ok = true;
+                            break;
+                        }
+                    }
+                    if (ok){
+                        i = j;
+                        flag = 1;
+                        // System.out.printf("%d ", i);
+                        break;
+                    }
+                    a[str.charAt(j) - 'a']++;
+                    str.deleteCharAt(str.length() - 1);
+                }
+                if (!ok)return "";
 
-        char center = 0;
-        for (int i = 0; i < 26; i++) {
-            if (freq[i] % 2 != 0) {
-                if (center != 0)
-                    return "";
-                center = (char) ('a' + i);
-                freq[i]--;
+            }
+            else {
+                for (int j = 0; j < 26; j++){
+                    if (a[j] > 0){
+                        a[j]--;
+                        str.append((char) (j + 'a'));
+                        break;
+                    }
+                }
+            }
+            
+        }
+        StringBuilder con = palind(str, mid);
+        if (comp(con, new StringBuilder(target)) == 1)return con.toString();
+        next_perm(str);
+        con = palind(str, mid);
+        if (comp(con, new StringBuilder(target)) == 1)return con.toString();
+        
+        return "";
+
+    }
+    int comp(StringBuilder s1, StringBuilder s2){
+        for (int i = 0; i < s1.length(); i++){
+            if (s1.charAt(i) == s2.charAt(i))continue;
+            return s1.charAt(i) < s2.charAt(i) ? -1 : 1;
+        }
+        return 0;
+    }
+    StringBuilder palind(StringBuilder s, char mid){
+        StringBuilder con = new StringBuilder();
+        con.append(s);
+        if (mid != '.') con.append(mid);
+        s.reverse();
+        con.append(s);
+        s.reverse();
+        return con;
+    }
+    void swap(StringBuilder s, int i, int j){
+        s.setCharAt(i, (char) (s.charAt(i) ^ s.charAt(j)));
+        s.setCharAt(j, (char) (s.charAt(i) ^ s.charAt(j)));
+        s.setCharAt(i, (char) (s.charAt(i) ^ s.charAt(j)));
+    }
+    void next_perm(StringBuilder s){
+        int n = s.length();
+        int pos = -1;
+        for (int i = n -2; i >= 0; i--){
+            if (s.charAt(i) < s.charAt(i + 1)){
+                pos = i;
+                break;
             }
         }
-
-        int sz = s.length();
-        int half = sz / 2;
-        for (int i = 0; i < half; i++)
-            freq[target.charAt(i) - 'a'] -= 2;
-
-        if (check(freq)) {
-            String head = target.substring(0, half);
-            String rev = new StringBuilder(head).reverse().toString();
-            String tail = "";
-            if (center != 0)
-                tail += center;
-            tail += rev;
-            if (tail.compareTo(target.substring(half)) > 0)
-                return head + tail;
-        }
-
-        for (int i = half - 1; i >= 0; i--) {
-            char w = target.charAt(i);
-            freq[w - 'a'] += 2;
-            if (!check(freq))
-                continue;
-
-            for (int j = (w - 'a') + 1; j < 26; j++) {
-                if (freq[j] == 0)
-                    continue;
-                freq[j] -= 2;
-                StringBuilder answer = new StringBuilder(target.substring(0, i + 1));
-                answer.setCharAt(i, (char) ('a' + j));
-
-                for (int k = 0; k < 26; k++) {
-                    int cnt = freq[k] / 2;
-                    for (int m = 0; m < cnt; m++)
-                        answer.append((char) ('a' + k));
+        if (pos == -1)return ;
+        for (int i = n - 1; i >= 0; i--){
+            if (s.charAt(i) > s.charAt(pos)){
+                swap(s, i, pos);
+                for (int j = pos + 1, r = n - 1; j < r; j++, r--){
+                    swap(s, j, r);
                 }
 
-                String part = new StringBuilder(answer).reverse().toString();
-                if (center != 0)
-                    answer.append(center);
-                answer.append(part);
-                return answer.toString();
+                break;
             }
         }
-
-        return "";
-    }
-
-    boolean check(int[] freq) {
-        for (int v : freq)
-            if (v < 0)
-                return false;
-        return true;
     }
 }
