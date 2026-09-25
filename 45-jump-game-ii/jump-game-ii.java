f1d7e4413e23dede53b3f2f47d368ebea42497d9
Rahul Asapu <asapurahul393@gmail.com>
2026-09-25 16:37:59 +0530

    Time: 0 ms (100.00%) | Memory: 47.5 MB (8.24%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.java
+++ 45-jump-game-ii/jump-game-ii.java
@@ -1,22 +1,31 @@
 class Solution {
+
     public int jump(int[] nums) {
-        int ans = 0;
-        int end = 0;
-        int farthest = 0;
+        int left = 0;
+        int right = 0;
+        int n = nums.length;
+        int count = 0;
 
-        for(int i = 0; i < nums.length - 1; ++i){
-            farthest = Math.max(farthest, i + nums[i]);
+        return helper(nums, left, right, n, count);
+    }
 
-            if(farthest >= nums.length - 1){
-                ++ans;
-                break;
-            }
+    public int helper(int[] nums, int left, int right, int n, int count) {
 
-            if(i == end){
-                ++ans;
-                end = farthest;
+        if (right >= n - 1) {
+            return count;
+        }
+
+        int max = 0;
+        int bestIndex = left;
+
+        for (int i = left; i <= right; i++) {
+
+            if (i + nums[i] > max) {
+                max = i + nums[i];
+                bestIndex = i;
             }
         }
-        return ans;
+
+        return helper(nums, bestIndex, max, n, count + 1);
     }
 }
